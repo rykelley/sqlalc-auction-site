8ed7371f47470824ddd752db4678db93e46fd3d0
Ryan Kelley <rykelley@gmail.com>
2015-07-19 19:01:07 -0600

    started engine build

---
+++ tbay.py
@@ -0,0 +1,12 @@
+__author__ = 'Ryan'
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('postgresql://ubuntu:thinkful@localhost:5432/tbay')
+Session = sessionmaker(bind=engine)
+session = Session()
+base = declarative_base()
+
+
