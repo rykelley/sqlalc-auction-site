5a2a8673e00e6c509079fd4a3e97200ef4da1bbb
Ryan Kelley <rykelley@gmail.com>
2015-07-19 19:20:56 -0600

    building first table

--- tbay.py
+++ tbay.py
@@ -3,10 +3,26 @@ __author__ = 'Ryan'
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
 
-engine = create_engine('postgresql://ubuntu:thinkful@localhost:5432/tbay')
+engine = create_engine('postgresql://root:Password!@104.131.148.235:5432/tbay')
 Session = sessionmaker(bind=engine)
 session = Session()
 base = declarative_base()
+
+
+class Item(base):
+    __tablename__ = "items"
+
+    id = Column(id, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    time = Column(datetime,default=datetime.utcnow)
+
+    
+
+
+
 
 
