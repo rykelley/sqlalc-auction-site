e3c83b5fb4eb69c011573b2f62747a5670a42d82
Ryan Kelley <rykelley@gmail.com>
2015-10-07 21:23:01 -0600

    finished intial models

--- tbay.py
+++ tbay.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime ,Float
 
 engine = create_engine('postgresql://root:Password!@104.131.148.235:5432/tbay')
 Session = sessionmaker(bind=engine)
@@ -15,12 +15,28 @@ base = declarative_base()
 class Item(base):
     __tablename__ = "items"
 
-    id = Column(id, primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     description = Column(String)
-    time = Column(datetime, default=datetime.utcnow)
+    time = Column(DateTime, default=datetime.utcnow)
 
-    Base.metadata.create_all(engine)
+
+class User(base):
+    __tablename__ = "users"
+
+    user_id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+
+
+class Bid(base):
+    __tablename__ = "Bids"
+
+    bid_id = Column(Integer, primary_key=True)
+    price = Column(Float, nullable=False)
+
+
+base.metadata.create_all(engine)
 
 
 
